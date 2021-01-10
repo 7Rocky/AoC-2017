7bf26274e55e5ab87e825aa248620d10adf2ed49
Rocky <rockygitt@gmail.com>
2021-01-10 02:02:59 +0100

    AoC Problem 08

--- day_08/main.py
+++ day_08/main.py
@@ -1,6 +1,28 @@
 import re
 
 
+def set_regs(regs, m):
+    reg, op, num_op = m.group(1), m.group(2), int(m.group(3))
+    reg_cond, cond, num_cond = m.group(4), m.group(5), int(m.group(6))
+
+    if op == 'dec':
+        num_op *= -1
+
+    if regs.get(reg) == None:
+        regs[reg] = 0
+    if regs.get(reg_cond) == None:
+        regs[reg_cond] = 0
+
+    if (cond == '==' and regs[reg_cond] == num_cond) \
+            or (cond == '!=' and regs[reg_cond] != num_cond) \
+            or (cond == '<=' and regs[reg_cond] <= num_cond) \
+            or (cond == '>=' and regs[reg_cond] >= num_cond) \
+            or (cond == '<' and regs[reg_cond] < num_cond) \
+            or (cond == '>' and regs[reg_cond] > num_cond):
+
+        regs[reg] += num_op
+
+
 def main():
     f = open('input.txt')
 
@@ -9,32 +31,10 @@ def main():
 
     for line in f:
         if m := re.match(r"(\w+) (inc|dec) (-?\d+) if (\w+) (==|!=|<=|>=|<|>) (-?\d+)", line):
-            reg, op, num_op = m.group(1), m.group(2), int(m.group(3))
-            reg_cond, cond, num_cond = m.group(4), m.group(5), int(m.group(6))
+            set_regs(regs, m)
 
-            if op == 'dec':
-                num_op *= -1
-
-            if regs.get(reg) == None:
-                regs[reg] = 0
-            if regs.get(reg_cond) == None:
-                regs[reg_cond] = 0
-
-            if cond == '==' and regs[reg_cond] == num_cond:
-                regs[reg] += num_op
-            elif cond == '!=' and regs[reg_cond] != num_cond:
-                regs[reg] += num_op
-            elif cond == '<=' and regs[reg_cond] <= num_cond:
-                regs[reg] += num_op
-            elif cond == '>=' and regs[reg_cond] >= num_cond:
-                regs[reg] += num_op
-            elif cond == '<' and regs[reg_cond] < num_cond:
-                regs[reg] += num_op
-            elif cond == '>' and regs[reg_cond] > num_cond:
-                regs[reg] += num_op
-
-            if max_value < max(regs.values()):
-                max_value = max(regs.values())
+        if max_value < max(regs.values()):
+            max_value = max(regs.values())
 
     f.close()
 
