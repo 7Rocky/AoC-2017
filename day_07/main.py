fb11cadeca391856a6f55730d515816628d83a4b
Rocky <rockygitt@gmail.com>
2021-01-20 21:35:53 +0100

    AoC Problem 07

--- day_07/main.py
+++ day_07/main.py
@@ -1,6 +1,6 @@
 def is_below(program, programs):
     for p in programs:
-        if program["name"] in p["above"]:
+        if program['name'] in p['above']:
             return False
 
     return True
@@ -8,7 +8,7 @@ def is_below(program, programs):
 
 def find_program(name, programs):
     for i, p in enumerate(programs):
-        if p["name"] == name:
+        if p['name'] == name:
             return p, i
 
     return None
@@ -17,21 +17,21 @@ def find_program(name, programs):
 def balance(name, programs):
     program, i = find_program(name, programs)
 
-    if program["total"] != 0:
-        return program["total"]
+    if program['total'] != 0:
+        return program['total']
 
-    if len(program["above"]) == 0:
-        program["total"] = program["weight"]
+    if len(program['above']) == 0:
+        program['total'] = program['weight']
         programs[i] = program
 
-        return program["total"]
+        return program['total']
 
-    total = program["weight"]
+    total = program['weight']
 
-    for above in program["above"]:
+    for above in program['above']:
         total += balance(above, programs)
 
-    program["total"] = total
+    program['total'] = total
     programs[i] = program
 
     return total
@@ -40,9 +40,9 @@ def balance(name, programs):
 def is_balanced(program, programs):
     weights = set()
 
-    for name in program["above"]:
+    for name in program['above']:
         above, _ = find_program(name, programs)
-        weights.add(above["total"])
+        weights.add(above['total'])
 
     return len(weights) < 2
 
@@ -50,9 +50,9 @@ def is_balanced(program, programs):
 def fix_weight(program, programs):
     weights = []
 
-    for name in program["above"]:
+    for name in program['above']:
         above, _ = find_program(name, programs)
-        weights.append(above["total"])
+        weights.append(above['total'])
 
     bad_weight, good_weight = 0, 0
 
@@ -62,11 +62,11 @@ def fix_weight(program, programs):
         else:
             good_weight = w
 
-    for name in program["above"]:
+    for name in program['above']:
         above, _ = find_program(name, programs)
 
-        if above["total"] == bad_weight:
-            return above["weight"] - (bad_weight - good_weight)
+        if above['total'] == bad_weight:
+            return above['weight'] - (bad_weight - good_weight)
 
 
 def main():
@@ -82,7 +82,7 @@ def main():
         if '->' in line:
             above = line.strip().split('-> ')[1].split(', ')
 
-        p = {"name": name, "weight": weight, "above": above, "total": 0}
+        p = {'name': name, 'weight': weight, 'above': above, 'total': 0}
         programs.append(p)
 
     f.close()
@@ -94,15 +94,15 @@ def main():
             below_program = p
             break
 
-    print(f'Bottom program name (1): { below_program["name"] }')
+    print(f"Bottom program name (1): { below_program['name'] }")
 
-    bad_program = {"total": 2 ** 32 - 1}
-
-    for p in programs:
-        balance(p["name"], programs)
+    bad_program = {'total': 2 ** 32 - 1}
 
     for p in programs:
-        if not is_balanced(p, programs) and p["total"] < bad_program["total"]:
+        balance(p['name'], programs)
+
+    for p in programs:
+        if not is_balanced(p, programs) and p['total'] < bad_program['total']:
             bad_program = p
 
     fixed_weight = fix_weight(bad_program, programs)
