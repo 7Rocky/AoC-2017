ece6d5430f7f54696011863bbd65dd14becdf313
Rocky <rockygitt@gmail.com>
2021-01-21 03:44:54 +0100

    AoC Problem 01

--- day_01/main.py
+++ day_01/main.py
@@ -1,11 +1,13 @@
 def solve(captcha, next):
-    return sum([int(captcha[i - next])
-                for i in range(len(captcha)) if captcha[i - next] == captcha[i]])
+    return sum(int(captcha[i - next])
+               for i in range(len(captcha)) if captcha[i - next] == captcha[i])
 
 
 def main():
     f = open('input.txt')
+
     captcha = f.readline().strip()
+
     f.close()
 
     print(f'Solution to captcha (1): { solve(captcha, 1) }')
