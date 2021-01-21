aef4aaf278a1b86cdd67f4458efccfe316f471a1
Rocky <rockygitt@gmail.com>
2021-01-21 02:33:16 +0100

    AoC Problem 11

---
+++ day_11/main.py
@@ -0,0 +1,42 @@
+steps = {
+    'ne': 1+1j,
+    'n': 2j,
+    'se': 1-1j,
+    'sw': -1-1j,
+    's': -2j,
+    'nw': -1+1j
+}
+
+
+def get_steps(pos):
+    num_steps = abs(pos.real)
+
+    if abs(pos.imag) - abs(pos.real) > 0:
+        num_steps += (abs(pos.imag) - abs(pos.real)) / 2
+
+    return int(num_steps)
+
+
+def main():
+    f = open('input.txt')
+
+    for line in f:
+        moves = [m for m in line.strip().split(',')]
+
+    f.close()
+
+    max_num_steps, pos = 0, 0
+
+    for s in [steps[m] for m in moves]:
+        pos += s
+        num_steps = get_steps(pos)
+
+        if max_num_steps < num_steps:
+            max_num_steps = num_steps
+
+    print(f'Number of steps to final position (1): { num_steps }')
+    print(f'Number of steps to furthest position (2): { max_num_steps }')
+
+
+if __name__ == '__main__':
+    main()
