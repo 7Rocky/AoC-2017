fd6eee77248cf25f28ee16b572bc98408cf709b4
Rocky <rockygitt@gmail.com>
2021-01-31 02:13:32 +0100

    AoC Problem 10

--- day_10/main.py
+++ day_10/main.py
@@ -23,6 +23,14 @@ def knot_round(numbers, start, length, skip_size):
     return numbers, (start + length + skip_size) % len(numbers)
 
 
+def round(lengths, numbers, start, skip_size):
+    for length in lengths:
+        numbers, start = knot_round(numbers, start, length, skip_size)
+        skip_size += 1
+
+    return start, skip_size
+
+
 def to_ascii(string):
     return [ord(c) for c in string]
 
@@ -35,6 +43,21 @@ def to_hex(numbers):
     return bytearray(numbers).hex()
 
 
+def get_knot_hash(lengths_string):
+    numbers = [n for n in range(256)]
+    rounds = 64
+    start, skip_size = 0, 0
+
+    lengths = to_ascii(lengths_string) + STANDARD_SUFFIX_VALUES
+
+    for _ in range(rounds):
+        start, skip_size = round(lengths, numbers, start, skip_size)
+
+    xors = [knot_hash(numbers[i * 16:(i + 1) * 16]) for i in range(16)]
+
+    return to_hex(xors)
+
+
 STANDARD_SUFFIX_VALUES = [17, 31, 73, 47, 23]
 
 
@@ -42,7 +65,6 @@ def main():
     f = open('input.txt')
 
     numbers = [n for n in range(256)]
-    lengths_string = ''
     lengths = []
 
     for line in f:
@@ -51,28 +73,11 @@ def main():
 
     f.close()
 
-    start, skip_size = 0, 0
-
-    for length in lengths:
-        numbers, start = knot_round(numbers, start, length, skip_size)
-        skip_size += 1
+    round(lengths, numbers, 0, 0)
 
     print(f'Result (1): { numbers[0] * numbers[1] }')
 
-    numbers = [n for n in range(256)]
-    rounds = 64
-    start, skip_size = 0, 0
-
-    lengths = to_ascii(lengths_string) + STANDARD_SUFFIX_VALUES
-
-    for _ in range(rounds):
-        for length in lengths:
-            numbers, start = knot_round(numbers, start, length, skip_size)
-            skip_size += 1
-
-    xors = [knot_hash(numbers[i * 16:(i + 1) * 16]) for i in range(16)]
-
-    print(f'Knot Hash (2): { to_hex(xors) }')
+    print(f'Knot Hash (2): { get_knot_hash(lengths_string) }')
 
 
 if __name__ == '__main__':
